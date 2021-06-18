a688d3b8d21d64344cfee17479ec27f6f0ce68ee
EdgardoP <edgardoportillo05@gmail.com>
2021-06-18 16:44:50 -0600

    Prueba 2

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hola esto es una prueba</Text>
+      <Text>Hola esto es una prueba 2</Text>
       <StatusBar style="auto" />
     </View>
   );
